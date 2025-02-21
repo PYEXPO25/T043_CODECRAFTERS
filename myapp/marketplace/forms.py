05745034c1f531cfd6bd789774a4235ec3649cc4
shakthiG06 <shakthi3176@gmail.com>
2025-02-21 16:37:22 +0530

    Form changed

--- myapp/marketplace/forms.py
+++ myapp/marketplace/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(forms.Form):
     username=forms.CharField(label='Username',max_length=20,required=True)
     email=forms.EmailField(label='Email',required=True)
     contact_number=forms.CharField(label='Contact Number',max_length=10,required=True)
+    
 
 class LoginForm(forms.Form):
     username=forms.CharField(label='Username',max_length=20,required=True)
