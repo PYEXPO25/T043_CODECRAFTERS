2612031a34e8560508fa5419c9d1386461be760c
Yashwini-13 <yashwiniseervi@gmail.com>
2025-02-21 14:36:29 +0530

    shop table

--- myapp/marketplace/models.py
+++ myapp/marketplace/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
+class District(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class ShopImages(models.Model):
+    image_1 = models.URLField(null=False)
+    image_2 = models.URLField(null=False)
+    image_3 = models.URLField(null=False)
+    image_4 = models.URLField(null=False)
+    image_5 = models.URLField(null=False)
+
 # Create your models here.
+class Shop(models.Model):
+    shop_name = models.CharField(max_length = 50)
+    district = models.ForeignKey(District,on_delete=models.CASCADE)
+    images = models.ForeignKey(ShopImages,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.shop_name
+
+
+
+
+
