2288018a7560bc70333b968af6403b5f04ea5c43
shakthiG06 <shakthi3176@gmail.com>
2025-02-21 14:26:02 +0530

    Register Form

--- myapp/marketplace/forms.py
+++ myapp/marketplace/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    username=forms.CharField(label='Name',max_length=20,required=True)
+    email=forms.EmailField(label='Email',required=True)
+    contact_number=forms.CharField(label='Contact Number',max_length=10,required=True)
